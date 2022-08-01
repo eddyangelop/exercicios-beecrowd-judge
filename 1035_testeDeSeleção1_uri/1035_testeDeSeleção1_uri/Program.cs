d99db2206529f966f82b3d8348aa5089d9dd06e2
eddyangelop <eddysoullive@gmail.com>
2022-08-01 20:39:40 -0300

    adicao de exercicios

---
+++ 1035_testeDeSeleção1_uri/1035_testeDeSeleção1_uri/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace teste_de_selecao_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;
+
+            String[] vet = Console.ReadLine().Split(' ');
+
+            a = int.Parse(vet[0]);
+            b = int.Parse(vet[1]);
+            c = int.Parse(vet[2]);
+            d = int.Parse(vet[3]);
+
+
+
+            if (b > c && d > a && c + d > a + b && c > 0 && d > 0 && a % 2 == 0)
+            {
+                Console.WriteLine("Valores aceitos");
+            }
+            else
+            {
+                Console.WriteLine("Valores nao aceitos");
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 1046_tempoDeJogo_uri/1046_tempoDeJogo_uri/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace tempo_de_jogo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] valores = Console.ReadLine().Split(' ');
+            int horaInicial = int.Parse(valores[0]);
+            int horaFinal = int.Parse(valores[1]);
+
+            int duracao;
+            if (horaInicial < horaFinal)
+            {
+                duracao = horaFinal - horaInicial;
+            }
+            else
+            {
+                duracao = 24 - horaInicial + horaFinal;
+            }
+
+            Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+
+            Console.ReadLine();
+        }
+    }
+}
+
