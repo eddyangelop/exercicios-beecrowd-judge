a13a9b7740443d578c8ccf31c9003ab89077d3bb
eddyangelop <eddysoullive@gmail.com>
2022-08-17 17:28:18 -0300

    adicao de exerc 17 08 22

---
+++ 1114_senhaFixa_uri/1114_senhaFixa_uri/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace senha_fixa
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+   
+
+            int senha = int.Parse(Console.ReadLine());
+
+     
+
+            while (senha != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+                senha = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("Acesso Permitido");
+            Console.ReadLine();
+
+        }
+    }
+}
+
