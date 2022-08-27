1c4f8413645c5be5f05cddd2e3aba353302c0cad
eddyangelop <eddysoullive@gmail.com>
2022-08-26 22:22:18 -0300

    adicao de exerc 26 08 22

---
+++ 1059_NúmerosPares_uri/1059_NúmerosPares_uri/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace numeros_pares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+
+            x = 100;
+
+            for (int i = 1; i <= x; i++)
+            {
+                if (i % 2 != 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
